239c617751f24bac11a0301aba3e9e81ce3df0f3
Mohamed El-Qarram <mohamed0ibrahim0000@gmail.com>
2025-11-30 14:09:05 +0200

    Adding deployment file for github actions

--- AtConnect.DAL/Repositories/ChatRepository.cs
+++ AtConnect.DAL/Repositories/ChatRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AtConnect.Core.Interfaces;
 using AtConnect.Core.Models;
 using AtConnect.DAL.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AtConnect.DAL.Repositories
 {
@@ -13,14 +14,24 @@ namespace AtConnect.DAL.Repositories
             this.appDbContext = appDbContext;
         }
 
-        public Task<Chat?> GetChatBetweenAsync(int userAId, int userBId)
+        public async Task<Chat?> GetChatBetweenAsync(int userAId, int userBId)
         {
-            throw new NotImplementedException();
+            return await appDbContext.Chats
+                .Include(c => c.User1)
+                .Include(c => c.User2)
+                .Include(c => c.Messages)
+                .FirstOrDefaultAsync(c => (c.User1Id == userAId && c.User2Id == userBId) || 
+                                          (c.User1Id == userBId && c.User2Id == userAId));
         }
 
-        public Task<IEnumerable<Chat>> GetUserChatsAsync(int userId)
+        public async Task<IEnumerable<Chat>> GetUserChatsAsync(int userId)
         {
-            throw new NotImplementedException();
+            return await appDbContext.Chats
+                .Include(c => c.User1)
+                .Include(c => c.User2)
+                .Include(c => c.Messages)
+                .Where(c => c.User1Id == userId || c.User2Id == userId)
+                .ToListAsync();
         }
     }
 }
