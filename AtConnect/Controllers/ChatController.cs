7963a8d7d865c711a0c1bc32ecd3e2960ee69a11
Mohamed El-Qarram <mohamed0ibrahim0000@gmail.com>
2025-12-04 17:05:19 +0200

    Merge chatRequest Controller with Chat Controller and create a new Controller for User-related endpoints

--- AtConnect/Controllers/ChatController.cs
+++ AtConnect/Controllers/ChatController.cs
@@ -1,5 +1,6 @@
 ﻿using AtConnect.BLL.DTOs;
 using AtConnect.BLL.Interfaces;
+using AtConnect.BLL.Services;
 using AtConnect.Core.Enum;
 using AtConnect.Core.Models;
 using AtConnect.Core.SharedDTOs;
@@ -13,14 +14,16 @@ namespace AtConnect.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class ChatController : ControllerBase
     {
         private readonly IChatService _chatService;
+        private readonly IRequestService _requestService;
 
-        public ChatController(IChatService chatService)
+        public ChatController(IChatService chatService, IRequestService requestService)
         {
             _chatService = chatService;
+            _requestService = requestService;
         }
         [HttpGet("UserChats")]
         public async Task<ActionResult<ResultDTO<List<UserChatDTO>>>> GetUserChats([FromQuery]  PaginationRequest request)
@@ -42,6 +45,25 @@ namespace AtConnect.Controllers
                 return Unauthorized(response);
             return response;
         }
+
+        [HttpPost("SendChatRequest")]
+        public async Task<ActionResult<ResultDTO<bool>>> SendRequest([FromBody] SendRequestDto dto)
+        {
+            if (dto == null) return BadRequest(new ResultDTO<bool>(false, "Invalid body."));
+
+            int.TryParse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value, out int senderId);
+
+            if (dto.ToUserId <= 0)
+                return BadRequest(new ResultDTO<bool>(false, "Couldn't find the user ID in the bearer token."));
+
+            var response = await _requestService.SendRequestAsync(senderId, dto.ToUserId);
+            if (!response.Success)
+                return BadRequest(response);
+
+            return response;
+        }
+
+
         [HttpPost("AcceptRequest")]
         public async Task<ActionResult<ResultDTO<object>>> ChangeChatRequestStatus(int RequestId, bool isAccepted)
         {
---
+++ AtConnect/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using AtConnect.BLL.DTOs;
+using AtConnect.BLL.Interfaces;
+using AtConnect.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AtConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("AllUsers")]
+        public async Task<ActionResult<ResultDTO<List<UserListItemDto>>>> GetUsers([FromQuery] PaginationRequest request)
+        {
+            int.TryParse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value, out int userId);
+            var response = await _userService.GetUsersAsync(userId, request.Page, request.PageSize);
+            if (!response.Success)
+                return BadRequest(response);
+            return response;
+        }
+
+    }
+
+}
